9317418dd2f3085dafd8d18f7d71b79fcfdfc2d4
Pro-man <ncr_30@yahoo.com>
2022-09-03 16:27:57 -0400

    terms initial commit.

---
+++ terms_notes.py
@@ -0,0 +1,84 @@
+
+# \n
+# \t
+# insert(0, 'ducati)
+# pop()
+# append()
+# del
+# rstrip()
+# lstrip()
+# strip()
+# upper()
+# lower()
+# title()
+# str()
+# remove()
+# sort()
+# sort(reverse=True)
+# sorted()
+# len()
+# reverse()
+# range()
+# == (compare)
+# != (not equal to)
+# items()
+# keys()
+# values()
+# set() - values to not repeat
+# input()
+# int() 
+# list()
+
+# while loop 
+# for loop
+# string
+# list
+# integers
+# float
+# order of operations
+# list comprehension
+# slicing a list 
+# looping through a list
+# copying a list
+# if statements 
+# if-else statments 
+# if-elif-else statement 
+# testing multiple conditionals 
+# boolean expressions
+# checking for inequality
+# numerical comparisons
+# checking multiple conditions
+# dictionary in dictionary 
+# list in dictionary 
+# flag 
+# break statement
+# continue statement
+# docstrings
+# function call 
+# parameter
+# argument
+# positional argument
+# keyword argument 
+# default value (default paramenter)
+# return value 
+# *asterisk (tuple)
+# ** 2-asterick (dictionary)
+# import module 
+
+# conditions keywords
+#   and 
+#   or 
+#   not equal 
+#   not in 
+#   in 
+
+# dictionary
+# function
+
+# type error
+# attribute error
+# index error
+# indentation error
+# logical error
+# syntax error
+# value error
